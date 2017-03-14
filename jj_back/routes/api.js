6175d0975dc075ac1ce83b0ded35e8c6358d4c5e
dirkdunn <dirkdunn1@gmail.com>
2017-03-13 22:20:12 -0500

    add route for TM api

--- jj_back/routes/api.js
+++ jj_back/routes/api.js
@@ -1,7 +1,19 @@
+/*
+TODO:
+  Connect to ticketmaster. Filter using city
+  Connect to twitter API
+  Filter tweets via coordinates
+  Run Tweets through Watson to get mood
+*/
+
+// TM standard API url
+// https://app.ticketmaster.com/discovery/v2/events.json?apikey=o3BJoz7MHeA4gc4KtbnDyaz2yr76GowY
+
 /*Dependencies*/
 var express = require('express');
 var router = express.Router();
 var watson = require('watson-developer-cloud');
+var request = require('request');
 
 var tone_analyzer = watson.tone_analyzer({
   username: process.env.WATSON_API_USERNAME,
@@ -11,7 +23,7 @@ var tone_analyzer = watson.tone_analyzer({
 });
 
 /* Text analyze */
-router.get('/', function(req, res, next) {
+router.get('/watson', function(req, res, next) {
   var textToAnalyze = req.body.analyze || 'Analyze this mudda bugga!!!!';
 
   tone_analyzer.tone({ text: textToAnalyze },
@@ -24,6 +36,31 @@ router.get('/', function(req, res, next) {
         console.log(JSON.stringify(tone, null, 2));
         res.json(tone);
       }
+  });
+});
+
+router.get('/ticketmaster',function(req,res){
+  //https://app.ticketmaster.com/discovery/v2/events.json?apikey=o3BJoz7MHeA4gc4KtbnDyaz2yr76GowY&city=Austin
+  // Austin360 Ampitheatre
+  /*
+
+  "longitude": "-97.64039993",
+  "latitude": "30.13310378"
+
+  requestBody._embedded.events (Array, ['_embedded']['venues']['location'])
+  */
+
+  var url ="https://app.ticketmaster.com/discovery/v2/events.json?apikey="+process.env.TM_API_KEY+"&city=Austin";
+  request(url, function (error, response, body) {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode);
+
+    if(error){
+      res.send(error)
+    } else {
+      var requestBody = JSON.parse(response.body);
+      res.json(requestBody['_embedded']['events']);
+    }
   });
 });
 
