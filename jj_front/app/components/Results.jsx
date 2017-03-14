e8460ee3074d1818d0f9660d67c7b877a4afbb97
Kyle Crichton <kylecrichton@gmail.com>
2017-03-14 18:10:10 -0500

    results boilerplate added

---
+++ jj_front/app/components/Results.jsx
@@ -0,0 +1,16 @@
+import React, { Component} from 'react';
+import Nav from 'nav';
+import router from 'app/router/';
+
+class Results extends Component {
+
+  render(){
+    return(
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Results;
