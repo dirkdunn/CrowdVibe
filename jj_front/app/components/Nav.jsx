3cc3bd18e96ec10099df7d645bfde1aa518fccac
Kollin Brandenburg <kollinbrandenburg@gmail.com>
2017-03-14 16:08:17 -0500

    changing the nav bar

--- jj_front/app/components/Nav.jsx
+++ jj_front/app/components/Nav.jsx
@@ -18,50 +18,13 @@ class Nav extends Component {
     return (
       <div className="container-fluid thenav">
         <div className="top-bar">
-          <nav role="navigation">
-            <div id="menuToggle">
-              <input type="checkbox"/>
-              <span></span>
-              <span></span>
-              <span></span>
-              <ul id="menu">
-                <a href="#/main">
-                  <li>Home</li>
-                </a>
-                <a href="#/blog">
-                  <li>Blog</li>
-                </a>
-                <a href="#/documentation">
-                  <li>Documentation</li>
-                </a>
-                <a href="#/settings">
-                  <li>Settings</li>
-                </a>
-                <a href="#/about">
-                  <li>About</li>
-                </a>
-              </ul>
-            </div>
-
-          </nav>
-
           <ul className="" data-dropdown-menu>
-              <li className="titlename navbar-left">Football-Data<div className="orgbold">.org</div></li>
-              <li className="navbar-right topright">
-                <Link to="/about" className="eachLink" activeClassName="active">About</Link>
+              <li className="titlename navbar-left">
+                <Link to="/main" className="eachLink" activeClassName="active">CrowdVibe</Link>
+
+
               </li>
-              <li className="navbar-right topright">
-                <Link to="/settings" className="eachLink" activeClassName="active">Settings</Link>
-              </li>
-              <li className="navbar-right topright">
-                <Link to="/documentation" className="eachLink" activeClassName="active">Docs</Link>
-              </li>
-              <li className="navbar-right topright">
-                <Link to="/blog" className="eachLink" activeClassName="active">Blog</Link>
-              </li>
-              <li className="navbar-right topright">
-                <Link to="/main" className="eachLink" activeClassName="active">Home</Link>
-              </li>
+
             </ul>
         </div>
 
