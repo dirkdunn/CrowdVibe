3b43428f697dd0344a38cd452324ff67c7fcb8b9
Kollin Brandenburg <kollinbrandenburg@gmail.com>
2017-03-14 23:19:32 -0500

    add events for front page

--- jj_front/app/components/Main.jsx
+++ jj_front/app/components/Main.jsx
@@ -7,7 +7,41 @@ import * as Redux from 'react-redux';
 import router from 'app/router/';
 
 class Main extends Component {
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      events: []
+    }
+  }
+
+  populateEvents(events){
+    // go through dom and add events
+    console.log('from main: ', events)
+    this.setState({
+      events: this.state.events.concat(events)
+    })
+  }
+
   render(){
+
+    let events = this.state.events.map((event,index)=>{
+      console.log(event.latitude,event.longitude)
+      // data="{latitude:"+event.latitude+", longitude: "+event.longitude+"}"
+      const coords = JSON.stringify({
+        latitude : event.latitude,
+        longitude : event.longitude
+      });
+      return (
+        <div  data-coords={coords} className="col-lg-4 card event" key={index}>
+          <h3>{event.name}</h3>
+          <img src={event.eventImage} alt="event"/>
+          <p>{event.info}</p>
+        </div>
+      )
+    });
+
     return (
       <div className="font-main">
         <Nav/>
@@ -19,29 +53,12 @@ class Main extends Component {
               <div className="titlesubtext">
                 Find Out What's Going On
               </div>
-              <SearchCity />
+              <SearchCity populateEvents={this.populateEvents.bind(this)}/>
             </div>
           </div>
           <div className="row">
             <div className="cardsection">
-              <div className="col-lg-4 card">
-                This is a Test of the card
-              </div>
-              <div className="col-lg-4 card">
-                This is a Test of the card
-              </div>
-              <div className="col-lg-4 card">
-                This is a Test of the card
-              </div>
-              <div className="col-lg-4 card">
-                This is a Test of the card
-              </div>
-              <div className="col-lg-4 card">
-                This is a Test of the card
-              </div>
-              <div className="col-lg-4 card">
-                This is a Test of the card
-              </div>
+              {events}
             </div>
           </div>
 
--- jj_front/app/components/SearchCity.js
+++ jj_front/app/components/SearchCity.js
@@ -6,20 +6,45 @@ import Request from 'superagent';
 
 
 class SearchCity extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      events: []
+    }
+  }
+
   onFormSubmit(){
     var url="http://localhost:3000/api/ticketmaster/";
     Request.post(url)
         .send({'city':'Chicago'})
         .set('Content-Type', 'application/x-www-form-urlencoded')
         .then((response) => {
-        console.log('Response from Twitter', response.body);
+        console.log('Response from Twitter', response);
+
+        // console.log(response.body);
+        response.body.forEach((event,index)=>{
+          this.setState({
+            events : this.state.events.concat({
+              name: event.name,
+              eventImage: event.images[0].url,
+              date: event.dates.start.localDate,
+              info: event.info,
+              latitude: event._embedded.venues[0].location.latitude,
+              longitude: event._embedded.venues[0].location.longitude
+            })
+          })
+        })
+
+        console.log(this.state.events);
+        this.props.populateEvents(this.state.events);
       });
   }
 
   render() {
     return (
       <div className="">
-        <form onSubmit={this.onFormSubmit}>
+        <form onSubmit={this.onFormSubmit.bind(this)}>
           <input className="searchcity center" type="search" ref="location" placeholder="Search for city by name"/>
           <button className="apibutton">Search For Vibes</button>
         </form>
