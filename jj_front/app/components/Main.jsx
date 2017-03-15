74db2fc417f76202d6ebffacdd133a7b4b45338c
Kollin Brandenburg <kollinbrandenburg@gmail.com>
2017-03-14 22:13:22 -0500

    working on the cards

--- jj_front/app/components/Main.jsx
+++ jj_front/app/components/Main.jsx
@@ -23,13 +23,29 @@ class Main extends Component {
             </div>
           </div>
           <div className="row">
-            <div className="col-lg-12 text-center mainlandingparagraph">
-              <h1>OUR PURPOSE</h1>
-              <div className="paragraph">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <div className="cardsection">
+              <div className="col-lg-4 card">
+                This is a Test of the card
+              </div>
+              <div className="col-lg-4 card">
+                This is a Test of the card
+              </div>
+              <div className="col-lg-4 card">
+                This is a Test of the card
+              </div>
+              <div className="col-lg-4 card">
+                This is a Test of the card
+              </div>
+              <div className="col-lg-4 card">
+                This is a Test of the card
+              </div>
+              <div className="col-lg-4 card">
+                This is a Test of the card
               </div>
             </div>
           </div>
+
+
         <Footer />
       </div>
     )
